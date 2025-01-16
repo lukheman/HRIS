e3239561328b3541688f4817cc21ae86c69046b7
Akmal <akmalthemalong@gmail.com>
2025-01-16 09:24:20 +0800

    update: template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,6 +31,25 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0"> {{ $subpage }}</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">{{ $page }}</a></li>
+                <li class="breadcrumb-item active">{{ $subpage }}</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
       @yield('content')
     </div>
     <!-- /.content-wrapper -->
