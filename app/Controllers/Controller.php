de1dd1a39d166701a6ed9f087afb8c630183ef76
Akmal <akmalthemalong@gmail.com>
2025-01-28 07:50:02 +0800

    update: add role

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -7,21 +7,25 @@ use eftec\bladeone\BladeOne;
 class Controller
 {
     protected $blade;
+    protected $role;
 
-    public function __construct(BladeOne $blade)
+    public function __construct(BladeOne $blade, $role = null)
     {
         $this->blade = $blade;
+        $this->role = $role;
     }
 
-    public function view($page, $data = null) {
+    public function view($page, $data = null)
+    {
 
-      if (isset($data)) {
-        echo $this->blade->run($page, $data);
+        if (isset($data)) {
+            $data['role'] = $this->role;
+            echo $this->blade->run($page, $data);
+            exit();
+        }
+
+        echo $this->blade->run($page);
         exit();
-      }
-
-      echo $this->blade->run($page);
-      exit();
 
     }
 
