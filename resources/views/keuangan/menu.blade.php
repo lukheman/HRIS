9ed8e9c09ca171748b2d8494e06314aafe4c42fb
Akmal <akmalthemalong@gmail.com>
2025-01-28 07:49:17 +0800

    add: absensi karyawan

--- resources/views/keuangan/menu.blade.php
+++ resources/views/keuangan/menu.blade.php
@@ -11,19 +11,17 @@
   <a href="@base_url(/keuangan/gaji-karyawan)" class="nav-link {{ $page === 'Gaji Karyawan' ? 'active' : ''}}">
     <i class="nav-icon fas fa-money-check-alt"></i>
     <p>
-      Gaji Karyawan
+      Laporan Gaji Karyawan
       <!-- <span class="right badge badge-danger">New</span> -->
     </p>
   </a>
 </li>
 
-<!--<li class="nav-item">-->
-<!--  <a href="@base_url(/keuangan/gajiKaryawan/verifikasiShow)"-->
-<!--    class="nav-link {{ $page === 'Verifikasi Gaji Karyawan' ? 'active' : ''}}">-->
-<!--    <i class="nav-icon fas fa-th"></i>-->
-<!--    <p>-->
-<!--      Verifikasi Gaji Karyawan-->
-<!--      <!-- <span class="right badge badge-danger">New</span> -->-->
-<!--    </p>-->
-<!--  </a>-->
-<!--</li>-->
+<li class="nav-item">
+  <a href="@base_url(/keuangan/absensi/all)" class="nav-link {{ $page === 'Absensi Karyawan' ? 'active' : ''}}">
+    <i class="nav-icon fas fa-calendar-check"></i>
+    <p>
+      Absensi Karyawan
+    </p>
+  </a>
+</li>
