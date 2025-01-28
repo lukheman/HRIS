16a527d92a49196aadd91126d2eafc4ef0a078d3
Akmal <akmalthemalong@gmail.com>
2025-01-28 18:09:05 +0800

    add: route

--- app/routes/auth.php
+++ app/routes/auth.php
@@ -3,6 +3,8 @@
 return function ($router) {
 
     $router->get('/', '\App\Controllers\HomeController', 'index');
+    $router->get('/absensi/scan', '\App\Controllers\HomeController', 'scanQrCode');
+    $router->post('/absensi/process-scan', '\App\Controllers\HomeController', 'processScan');
     $router->get('/login', '\App\Controllers\AuthController', 'showLogin');
     $router->post('/login', '\App\Controllers\AuthController', 'handleLogin');
     $router->get('/logout', '\App\Controllers\AuthController', 'logout');
