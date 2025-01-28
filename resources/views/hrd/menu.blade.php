536bbf12b26adb99340020b762b83e3a70e6c1de
Akmal <akmalthemalong@gmail.com>
2025-01-28 16:40:04 +0800

    update: add gaji karyawna

--- resources/views/hrd/menu.blade.php
+++ resources/views/hrd/menu.blade.php
@@ -16,20 +16,20 @@
   </a>
 </li>
 
-<!--<li class="nav-item">-->
-<!--  <a href="@base_url(/hrd/karyawanAddForm)" class="nav-link {{ $page === 'karyawanAdd' ? 'active' : ''}}">-->
-<!--    <i class="nav-icon fas fa-th"></i>-->
-<!--    <p>-->
-<!--      Tambah Karyawan-->
-<!--    </p>-->
-<!--  </a>-->
-<!--</li>-->
-
 <li class="nav-item">
   <a href="@base_url(/hrd/absensi/all)" class="nav-link {{ $page === 'Absensi Karyawan' ? 'active' : ''}}">
     <i class="nav-icon fas fa-calendar-check"></i>
     <p>
       Absensi Karyawan
+    </p>
+  </a>
+</li>
+
+<li class="nav-item">
+  <a href="@base_url(/hrd/gaji-karyawan)" class="nav-link {{ $page === 'Gaji Karyawan' ? 'active' : ''}}">
+    <i class="nav-icon fas fa-money-check-alt"></i>
+    <p>
+      Laporan Gaji
     </p>
   </a>
 </li>
@@ -51,24 +51,3 @@
     </p>
   </a>
 </li>
-
-<!-- <li class="nav-item"> -->
-<!--   <a href="@base_url(/hrd/calendar)" class="nav-link {{ $page === 'absensi' ? 'active' : ''}}"> -->
-<!--     <i class="nav-icon fas fa-th"></i> -->
-<!--     <p> -->
-<!--       Kalender -->
-<!-- <span class="right badge badge-danger">New</span> -->
-<!--     </p> -->
-<!--   </a> -->
-<!-- </li> -->
-
-<!-- <li class="nav-item"> -->
-<!--   <a href="@base_url(hrd.php?page=karyawan-absensi-bulan)" -->
-<!--     class="nav-link {{ $page === 'karyawan-absensi-bulan' ? 'active' : ''}}"> -->
-<!--     <i class="nav-icon fas fa-th"></i> -->
-<!--     <p> -->
-<!--       Absensi Perbulan -->
-<!-- <span class="right badge badge-danger">New</span> -->
-<!--     </p> -->
-<!--   </a> -->
-<!-- </li> -->
