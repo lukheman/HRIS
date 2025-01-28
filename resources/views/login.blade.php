3c868f6a663a15a12df553429f3b7484421a338a
Akmal <akmalthemalong@gmail.com>
2025-01-28 16:44:52 +0800

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -52,16 +52,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div>
           </div>
           <div class="row">
-            <div class="col-8">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember" disabled>
-                <label for="remember">
-                  Remember Me
-                </label>
-              </div>
-            </div>
-            <!-- /.col -->
-            <div class="col-4">
+            <div class="col-12">
               <button type="submit" class="btn btn-primary btn-block">Sign In</button>
             </div>
             <!-- /.col -->
@@ -78,9 +69,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- </div> -->
         <!-- /.social-auth-links -->
 
-        <p class="mb-1">
-          <a href="forgot-password.html">I forgot my password</a>
-        </p>
+        <!--<p class="mb-1">-->
+        <!--  <a href="forgot-password.html">I forgot my password</a>-->
+        <!--</p>-->
+
         <!-- <p class="mb-0"> -->
         <!--   <a href="register.html" class="text-center">Register a new membership</a> -->
         <!-- </p> -->
