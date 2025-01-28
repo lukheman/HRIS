bc0ec854995e4f82a2ce4e039f5c8e24029ee4d2
Akmal <akmalthemalong@gmail.com>
2025-01-28 16:39:50 +0800

    new: features

---
+++ resources/views/features/gajiKaryawan/detailGajiKaryawan.blade.php
@@ -0,0 +1,266 @@
+@extends('layouts.main')
+
+@section('title', strtoupper($role))
+
+@section('sidebar-menu')
+
+@include($role . '.menu')
+
+@endsection
+
+@section('content')
+
+
+<!-- Main content -->
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="card">
+          <div class="card-header">
+
+            <div class="row">
+              <div class="col-6">
+                <!-- <a class="btn btn-primary float-end" href="@base_url(/{{ $role }}/karyawan/add)">Tambah Laporan Gaji</a> -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Tambah
+                  Laporan Gaji</button>
+              </div>
+              <div class="col-6">
+                <p class="font-weight-bold float-right">{{ $namaKaryawan }}</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- /.card-header -->
+          <div class="card-body">
+            <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+              <div class="row">
+                <div class="col-lg-12">
+                  <table id="datatable" class="table table-bordered table-striped dataTable dtr-inline collapsed"
+                    aria-describedby="datatable_info">
+                    <thead>
+                      <tr>
+                        <!--     <th class="sorting sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" -->
+                        <!--       aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending"> -->
+                        <!-- </th> -->
+                        <!--     <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" -->
+                        <!--       aria-label="Browser: activate to sort column ascending">Browser</th> -->
+                        <!--     <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" -->
+                        <!--       aria-label="Platform(s): activate to sort column ascending">Platform(s)</th> -->
+                        <!--     <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" -->
+                        <!--       aria-label="Engine version: activate to sort column ascending">Engine version</th> -->
+                        <!-- <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" -->
+                        <!--       aria-label="CSS grade: activate to sort column ascending" style="display: none;">CSS grade -->
+                        <!--     </th> -->
+                        <!--    <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">-->
+                        <!--ID Karyawan</th>-->
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Periode</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Gaji Pokok (Rp.)</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Gaji Lembur (Rp.)</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Total Durasi Lembur (Jam)</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Total Gaji (Rp.)</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Status</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Tindakan</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                      @foreach ($dataGajiKaryawan as $gaji)
+                      <tr>
+                        <td>{{ $gaji->periode}}</td>
+                        <td>{{ number_format($gaji->gaji_pokok, 2, ',', '.')}}</td>
+                        <td>{{ number_format($gaji->gaji_lembur, 2, ',', '.')}}</td>
+                        <td>{{ $gaji->total_lembur}}</td>
+                        <td>{{ number_format($gaji->gaji_total, 2, ',', '.')}}</td>
+                        <td>
+                          @if ($gaji->status === 'PENDING')
+                          <span class="badge bg-warning">{{ $gaji->status }}</span>
+                          @elseif ($gaji->status === 'DISETUJUI')
+                          <span class="badge bg-success">{{ $gaji->status }}</span>
+                          @elseif ($gaji->status === 'DITOLAK')
+                          <span class="badge bg-danger">{{ $gaji->status }}</span>
+                          @endif
+                        </td>
+                        <td>
+                          <div class="btn-group w-100">
+
+                            <!-- <form action="@base_url(/{{ $role }}/karyawan/update)" method="get"> -->
+                            <!-- <input type="hidden" name="id" value="{{ $karyawan->id }}"> -->
+                            <!-- <button type="submit" class="btn  btn-sm btn-primary">Update</button> -->
+                            <!-- </form> -->
+
+                            <button type="button" class="btn btn-sm btn-danger btn-delete-laporan"
+                              data-id="{{ $gaji->id }}">
+                              <i class="nav-icon fas fa-trash"></i>
+                            </button>
+
+                          </div>
+                        </td>
+                      </tr>
+                      @endforeach
+
+
+
+                    </tbody>
+
+                    <!-- <tfoot> -->
+                    <!--   <tr> -->
+                    <!--     <th rowspan="1" colspan="1">Rendering engine</th> -->
+                    <!--     <th rowspan="1" colspan="1">Browser</th> -->
+                    <!--     <th rowspan="1" colspan="1">Platform(s)</th> -->
+                    <!--     <th rowspan="1" colspan="1">Engine version</th> -->
+                    <!--     <th rowspan="1" colspan="1" style="display: none;">CSS grade</th> -->
+                    <!--   </tr> -->
+                    <!-- </tfoot> -->
+
+                  </table>
+                </div>
+              </div>
+              <!-- <div class="row"> -->
+              <!--   <div class="col-sm-12 col-md-5"> -->
+              <!--     <div class="dataTables_info" id="datatable_info" role="status" aria-live="polite">Showing 1 to 10 of 57 -->
+              <!--       entries</div> -->
+              <!--   </div> -->
+              <!--   <div class="col-sm-12 col-md-7"> -->
+              <!--     <div class="dataTables_paginate paging_simple_numbers" id="datatable_paginate"> -->
+              <!--       <ul class="pagination"> -->
+              <!--         <li class="paginate_button page-item previous disabled" id="datatable_previous"><a href="#" -->
+              <!--             aria-controls="datatable" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li> -->
+              <!--         <li class="paginate_button page-item active"><a href="#" aria-controls="datatable" data-dt-idx="1" -->
+              <!--             tabindex="0" class="page-link">1</a></li> -->
+              <!--         <li class="paginate_button page-item "><a href="#" aria-controls="datatable" data-dt-idx="2" -->
+              <!--             tabindex="0" class="page-link">2</a></li> -->
+              <!--         <li class="paginate_button page-item "><a href="#" aria-controls="datatable" data-dt-idx="3" -->
+              <!--             tabindex="0" class="page-link">3</a></li> -->
+              <!--         <li class="paginate_button page-item "><a href="#" aria-controls="datatable" data-dt-idx="4" -->
+              <!--             tabindex="0" class="page-link">4</a></li> -->
+              <!--         <li class="paginate_button page-item "><a href="#" aria-controls="datatable" data-dt-idx="5" -->
+              <!--             tabindex="0" class="page-link">5</a></li> -->
+              <!--         <li class="paginate_button page-item "><a href="#" aria-controls="datatable" data-dt-idx="6" -->
+              <!--             tabindex="0" class="page-link">6</a></li> -->
+              <!--         <li class="paginate_button page-item next" id="datatable_next"><a href="#" aria-controls="datatable" -->
+              <!--             data-dt-idx="7" tabindex="0" class="page-link">Next</a></li> -->
+              <!--       </ul> -->
+              <!--     </div> -->
+              <!--   </div> -->
+              <!-- </div> -->
+            </div>
+          </div>
+          <!-- /.card-body -->
+        </div>
+
+      </div>
+      <!-- /.col-md-6 -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+  <div class="modal fade" id="modal-default" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Tambah Laporan Gaji</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <form action="@base_url(/{{$role}}/gaji-karyawan/add)" method="post">
+          <div class="modal-body">
+
+            <input type="hidden" name="id" value="{{ $idKaryawan }}">
+
+            <div class="form-group">
+              <label for="durasi-lembur">Durasi Lembur (Jam)</label>
+              <input type="number" class="form-control" id="durasi-lembur" name="durasi_lembur"
+                placeholder="Total Durasi Lembur" min="0">
+            </div>
+
+            <div class="form-group">
+              <label for="gaji-lembur">Gaji Lembur</label>
+              <input type="number" class="form-control" id="gaji-lembur" name="gaji_lembur" placeholder="Gaji Lembur"
+                min="0">
+            </div>
+
+            <div class="form-group">
+              <label for="periode">Periode (Tahun-Bulan)</label>
+              <input type="month" class="form-control" id="periode" name="periode" placeholder="periode" required>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+            <button type="submit" class="btn btn-primary">Tambahkan</button>
+          </div>
+        </form>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+</div>
+<!-- /.content -->
+
+<script>
+
+  $(document).ready(() => {
+
+    $(".btn-delete-laporan").on('click', async function () {
+
+      Swal.fire({
+        customClass: {
+          confirmButton: "btn btn-danger",
+          // cancelButton: "btn btn-danger"
+        },
+        title: "Anda yakin untuk menghapus data laporan?",
+        // text: "Proses ini akan menghapus semua data absensi dan gaji karyawan!",
+        // showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Hapus Data",
+        // denyButtonText: `Don't save`
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          try {
+            const response = await fetch('@base_url(/{{ $role }}/gaji-karyawan/delete)', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                id: $(this).data('id')
+              })
+            })
+            if (!response.ok) {
+              const errorData = await response.json();
+              throw new Error(errorData.message || 'Terjadi kesalahan');
+            }
+
+            if (!response.ok) {
+              throw new Error(errorData.message || 'Terjadi kesalahan');
+              Swal.fire("Data gagal dihapus!", "", "danger");
+            } else {
+              Swal.fire("Data berhasil dihapus!", "", "success").then(() =>
+                location.reload());
+              d
+            }
+
+          } catch (error) {
+            console.error('Error:', error);
+          }
+
+        }
+      });
+
+
+    });
+  })
+
+</script>
+
+@endsection
---
+++ resources/views/features/listAbsensi.blade.php
@@ -0,0 +1,192 @@
+@extends('layouts.main')
+
+@section('title', strtoupper($role))
+
+@section('sidebar-menu')
+
+@include($role . '.menu')
+
+@endsection
+
+@section('content')
+
+<!-- TODO: tambahkan pageination -->
+
+<div class="content">
+  <div class="contaier-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <div class="btn-group w-100">
+              <a class="btn btn-primary {{ $by === 'all' ? 'disabled' : 'active' }}"
+                href="@base_url(/{{$role }}/absensi/all)"> <i class="nav-icon fas fa-border-all"></i>
+                Semua</a>
+              <a href="@base_url(/{{$role}}/absensi/all?by=day)"
+                class="btn btn-primary {{ $by === 'day' ? 'disabled' : 'active' }}">
+                <i class="nav-icon fas fa-calendar-day"></i>
+                Hari Ini</a>
+              <a class="btn btn-primary {{ $by === 'month' ? 'disabled' : 'active' }}"
+                href="@base_url(/{{$role }}/absensi/all?by=month)"> <i class="nav-icon fas fa-calendar-week"></i>
+                Bulan Ini</a>
+            </div>
+
+          </div>
+          <div class="card-body">
+            <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+              <div class="row">
+                <div class="col-12">
+                  <table id="datatable" class="table table-bordered table-stripped dataTable dtr-inline collapsed"
+                    aria-describedby="datatable_info">
+                    <thead>
+                      <tr>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Nama</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Tanggal</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Jam Masuk</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Jam Keluar</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Lembur (Jam)</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Status</th>
+                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1">
+                          Tindakan</th>
+                      </tr>
+
+                    </thead>
+                    <tbody>
+
+                      @foreach ($data_absensi as $absensi)
+                      <tr>
+                        <td>{{ $absensi->nama}}</td>
+                        <td>{{ $absensi->tanggal}}</td>
+                        <td>{{ $absensi->jam_masuk}}</td>
+                        <td>{{ $absensi->jam_keluar}}</td>
+                        <td>{{ $absensi->lembur}}</td>
+                        <td>
+                          @if ($absensi->status === 'Hadir')
+                          <span class="badge bg-success">{{ $absensi->status }}</span>
+                          @elseif ($absensi->status === 'Alpha')
+                          <span class="badge bg-danger">{{ $absensi->status }}</span>
+                          @endif
+                        </td>
+                        <td>
+
+
+                          <div class="btn-group">
+
+                            <a href="@base_url(/{{ $role }}/absensi/detail?id={{ $absensi->karyawan_id }}&periode={{ date('Y-m')}})"
+                              class="btn btn-sm btn-outline-primary">
+                              <i class="nav-icon fas fa-calendar-alt"></i> Detail
+                            </a>
+
+                            <!-- <a href="@base_url(/{{ $role }}/absensi/update?id_absensi={{ $absensi->id }})" -->
+                            <!--   class="btn btn-sm btn-outline-primary"> -->
+                            <!--   <i class="nav-icon fas fa-pencil-alt"></i> Edit -->
+                            <!-- </a> -->
+
+                            <button type="button" class="btn btn-sm btn-outline-primary btn-edit" data-toggle="modal"
+                              data-target="#modal-default" data-id="{{ $absensi->id_absensi }}">
+                              <i class="nav-icon fas fa-pencil-alt"></i> Edit
+                            </button>
+
+                          </div>
+
+                        </td>
+
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="modal-default" style="display: none;" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Edit Absensi</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <form>
+        <div class="modal-body">
+
+          <input type="hidden" name="id_absensi">
+
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" placeholder="Nama" name="nama" disabled>
+          </div>
+
+          <div class="form-group">
+            <label for="lembur">Durasi Lembur (Jam)</label>
+            <input type="number" class="form-control" id="lembur" placeholder="Lembur" min="0" name="durasi_lembur">
+          </div>
+
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" type="submit" id="btn-submit">Simpan</button>
+        </div>
+      </form>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+
+<script>
+
+  $(document).ready(() => {
+
+    $('.btn-edit').click(async function () {
+
+      var row = $(this).closest("tr");
+
+      // Ambil nama dari kolom pertama di baris tersebut
+      var nama = row.find("td:first").text();
+
+      // Opsional: Isi input di modal dengan nama yang diambil
+      $("#modal-default input[name='nama']").val(nama);
+      $("#modal-default input[name='id_absensi']").val($(this).data('id'));
+
+    })
+  })
+
+  $('#btn-submit').click(function (e) {
+    e.preventDefault();
+
+    let id_absensi = $("#modal-default input[name='id_absensi']").val();
+    let durasi_lembur = $("#modal-default input[name='durasi_lembur']").val();
+
+    $.ajax({
+      url: '@base_url(/{{ $role }}/absensi/update)',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({id_absensi: id_absensi, durasi_lembur: durasi_lembur}),
+      success: function (response) {
+        Swal.fire("Data absensi berhasil diupdate", "", "success").then(() => location.reload());
+      },
+      error: function (xhr, status, error) {
+        // Tampilkan pesan error
+        console.error("Terjadi kesalahan:", error);
+        Swal.fire("Data absensi gagal diupdate", "", "danger");
+      },
+    })
+
+  })
+
+</script>
+@endsection
