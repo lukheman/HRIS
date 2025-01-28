e83eb0e89db2fdbc8a8af030de659a729bede4f0
Akmal <akmalthemalong@gmail.com>
2025-01-28 20:58:55 +0800

    add: routers

--- app/routes/keuangan.php
+++ app/routes/keuangan.php
@@ -16,6 +16,8 @@ return function ($router) {
         $router->post('/gaji-karyawan/delete', $controller, 'deleteGajiKaryawan');
         $router->post('/gaji-karyawan/approve', $controller, 'approveGajiKaryawan');
         $router->post('/gaji-karyawan/pending', $controller, 'pendingGajiKaryawan');
+        $router->post('/gaji-karyawan/approve-selected', $controller, 'approveSelectedGajiKaryawan');
+        $router->post('/gaji-karyawan/pending-selected', $controller, 'pendingSelectedGajiKaryawan');
 
         $router->post('/gaji-karyawan/cetak-slip-gaji', $controller, 'cetakSlipGajiOne');
         $router->get('/gaji-karyawan/cetak-slip-gaji-all', $controller, 'cetakSlipGajiAll');
