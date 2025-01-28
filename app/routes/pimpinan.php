ff4181538d79f0c14c0a49b8f898770d8b8965dd
Akmal <akmalthemalong@gmail.com>
2025-01-28 16:40:17 +0800

    update: add routes

--- app/routes/pimpinan.php
+++ app/routes/pimpinan.php
@@ -5,8 +5,18 @@ return function ($router) {
         $controller = '\App\Controllers\PimpinanController';
 
         $router->get('', $controller, 'index');
-        $router->get('/gaji-karyawan', $controller, 'listGajiKaryawan');
-        $router->get('/gaji-karyawan/update', $controller, 'updateGajiKaryawan');
+        $router->get('/', $controller, 'index');
+
+        // $router->get('/gaji-karyawan', $controller, 'listGajiKaryawan');
+        // $router->get('/gaji-karyawan/update', $controller, 'updateGajiKaryawan');
+
+        $router->get('/gaji-karyawan', $controller, 'selectKaryawanGaji');
+        $router->get('/gaji-karyawan/detail', $controller, 'detailGajiKaryawan');
+
+        // $router->post('/gaji-karyawan/add', $controller, 'addGajiKaryawan');
+        // $router->post('/gaji-karyawan/delete', $controller, 'deleteGajiKaryawan');
+        // $router->post('/gaji-karyawan/approve', $controller, 'approveGajiKaryawan');
+        // $router->post('/gaji-karyawan/pending', $controller, 'pendingGajiKaryawan');
 
         $router->post('/gaji-karyawan/cetak-slip-gaji', $controller, 'cetakSlipGajiOne');
         $router->get('/gaji-karyawan/cetak-slip-gaji-all', $controller, 'cetakSlipGajiAll');
