e7e7d3a019f68061ef67cdc9bb9681cc260f146a
Akmal <akmalthemalong@gmail.com>
2025-01-29 14:45:00 +0800

    update: add comment

--- app/routes/hrd.php
+++ app/routes/hrd.php
@@ -2,21 +2,22 @@
 
 return function ($router) {
 
-
     $router->group('/hrd', ['Auth', 'HRDAccess'], function ($router) {
         $controller = '\App\Controllers\HrdController';
 
+        // Route untuk halaman utama HRD
         $router->get('', $controller, 'index');
         $router->get('/', $controller, 'index');
-        $router->get('/karyawan', $controller, 'listKaryawan');
 
+        // route untuk manajemen karyawan
+        $router->get('/karyawan', $controller, 'listKaryawan');
         $router->get('/karyawan/add', $controller, 'addKaryawanForm');
         $router->post('/karyawan/add', $controller, 'createKaryawan');
-
         $router->get('/karyawan/update', $controller, 'updateKaryawanForm');
         $router->post('/karyawan/update', $controller, 'updateKaryawan');
         $router->post('/karyawan/delete', $controller, 'deleteKaryawan');
 
+        // route untuk managemen gaji karyawan
         $router->get('/gaji-karyawan', $controller, 'selectKaryawanGaji');
         $router->get('/gaji-karyawan/detail', $controller, 'detailGajiKaryawan');
         $router->post('/gaji-karyawan/add', $controller, 'addGajiKaryawan');
@@ -25,10 +26,12 @@ return function ($router) {
         $router->post('/gaji-karyawan/cetak-slip-gaji', $controller, 'cetakSlipGajiOne');
         $router->get('/gaji-karyawan/cetak-slip-gaji-all', $controller, 'cetakSlipGajiAll');
 
+        // route untuk absensi
         $router->get('/absensi/detail', $controller, 'absensiBulanan');
         $router->get('/absensi/all', $controller, 'listAbsensi');
         $router->post('/absensi/update', $controller, 'updateAbsensi');
 
+        // route untuk qrcode
         $router->get('/absensi/scan-qrcode', $controller, 'scanQrCode');
         $router->get('/absensi/generate-qrcode', $controller, 'generateQrCode');
         $router->get('/absensi/generate-qrcode/search', $controller, 'generateQrCode');
