5ac7904b32039f768019a792676f3da1bc5cf06e
Akmal <akmalthemalong@gmail.com>
2025-01-31 16:40:36 +0800

    feat: menambahkan handler untuk mendapatkan daftar jabatan dan gaji

--- app/Controllers/Api/KaryawanApiController.php
+++ app/Controllers/Api/KaryawanApiController.php
@@ -3,14 +3,17 @@
 namespace App\Controllers\Api;
 
 use App\Models\KaryawanModel;
+use App\Models\JabatanModel;
 
 class KaryawanApiController
 {
     private $karyawanModel;
+    private $jabatanModel;
 
     public function __construct()
     {
         $this->karyawanModel = new KaryawanModel();
+        $this->jabatanModel = new JabatanModel();
     }
 
     public function getKaryawanAll()
@@ -21,6 +24,18 @@ class KaryawanApiController
           'status' => 'success',
           'message' => 'successfully retrieved all employee data',
           'data' => $listKaryawan,
+        ];
+
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+
+    public function getJabatan() {
+        header('Content-Type: application/json');
+        $data = $this->jabatanModel->all();
+        $response = [
+          'status' => 'success',
+          'message' => 'successfully retrieved all jabatan data',
+          'data' => $data,
         ];
 
         echo json_encode($response, JSON_PRETTY_PRINT);
