4f7d06974e22c25f454d35dae38c75c19581942a
Akmal <akmalthemalong@gmail.com>
2025-02-01 16:40:09 +0800

    feat(fitur): tambah fitur untuk mengenerate header untuk laporan absensi

--- app/Utils/Util.php
+++ app/Utils/Util.php
@@ -23,3 +23,19 @@ function addEndDate($periode) {
     return $tanggalAkhir;
 }
 
+function generateHeaderDate($periode) {
+      list($tahun, $bulan) = explode('-', $periode);
+
+    // Hitung jumlah hari dalam bulan
+    $jumlahHari = cal_days_in_month(CAL_GREGORIAN, $bulan, $tahun);
+
+    // Array nama hari dalam bahasa Indonesia
+    $namaHari = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+
+    // Generate array tanggal dari 01 hingga jumlah hari
+    $tanggalHeader = array_map(function($hari) {
+        return str_pad($hari, 2, '0', STR_PAD_LEFT); // Format dua digit (01, 02, dst.)
+    }, range(1, $jumlahHari));
+
+    return $tanggalHeader;
+}
